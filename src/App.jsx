f6dba8e136aa7d07c17afd8a288abbe683225b31
Franklin Guevara <tecguevara@gmail.com>
2025-03-31 22:04:36 -0600

    learned how to create new components, import them into a main.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [message, setName] = useState('') // State to store the input value
+  const isAdmin = true; // Define isAdmin as a constant or state
+  const userRole  = isAdmin ? 'admin' : 'guest'; // Conditional assignment of userRole
+
+  const handleClick = () => {
+    alert("You are " + userRole) // Alert with the input value
+    setName('') // Clear the input field
+  }
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="text"
+        id="name"
+        value={message} // Bind the input value to state
+        onChange={(e) => setName(e.target.value)} // Update state on input change
+      />
+      <button onClick={handleClick}>Click me</button>
     </>
   )
 }
-
 export default App
